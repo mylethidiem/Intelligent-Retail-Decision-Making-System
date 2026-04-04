53803c19df3c126b59dad8d7bf9c02844f72de09
Elizabeth Myn (banhmuy) <lethidiemmy961996@gmail.com>
2026-04-04 18:37:50 +0700

    Setup FastAPI-Gradio stack with Docker support (#8)
    * Run by Docker
    
    * Remove import env in docker compose
    
    * Ignore copy env when run docker

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,35 @@
-FROM python:3.11
+FROM python:3.11-slim
 
 WORKDIR /app
 
 # Install system dependencies
-RUN apt-get update && apt-get install -y \
+# RUN apt-get update && apt-get install -y \
+#     libgl1 \
+#     libglib2.0-0 \
+#     libsm6 \
+#     libxext6 \
+#     libxrender-dev \
+#     && rm -rf /var/lib/apt/lists/*
+
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    libgl1 \
     libglib2.0-0 \
     libsm6 \
     libxext6 \
     libxrender-dev \
+    && apt-get clean \
     && rm -rf /var/lib/apt/lists/*
+
+# Install Torch CPU first (Very important to reduce size)
+RUN pip install --no-cache-dir torch torchvision --index-url https://download.pytorch.org/whl/cpu
 
 # Copy requirements and install Python dependencies
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
+
+# Set UTF-8 encoding
+ENV LANG C.UTF-8
+ENV LC_ALL C.UTF-8
 
 # Copy application code
 COPY . .
--- app.sh
+++ app.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-# --- CẤU HÌNH API BACKEND (UVICORN) ---
+# --- API BACKEND (UVICORN) Config ---
 
 echo "Starting Uvicorn API Backend on port 5050..."
 
-# Sử dụng toán tử '&' để chạy lệnh Uvicorn ở chế độ nền (background)
-# Điều này cho phép script tiếp tục chạy lệnh tiếp theo ngay lập tức.
 uvicorn app.main:app --host 0.0.0.0 --port 5050 &
 
-# CẢNH BÁO: Rất quan trọng! Thêm độ trễ để API có thời gian khởi động
-# trước khi frontend cố gắng kết nối. Điều này ngăn lỗi "Connection refused".
+
 echo "Waiting 10 seconds for API to initialize..."
 sleep 10
 
-# --- CẤU HÌNH FRONTEND (GRADIO) ---
+# --- FRONTEND (GRADIO) ---
 
 echo "Starting Gradio Frontend..."
 
-# Lệnh này sẽ chạy ở chế độ foreground (tiền cảnh)
-# và giữ cho container/Space không bị thoát.
+# This command will run in the foreground
+# and keep the container/Space from exiting.
 python -m app.frontend.gradio_ui
 
-# Nếu Gradio của bạn được khởi chạy bằng launch(server_port=7860),
-# thì lệnh này sẽ chiếm Terminal và giữ cho ứng dụng chạy.
+# If your Gradio is launched with launch(server_port=7860),
+# this command will occupy the Terminal and keep the app running.
